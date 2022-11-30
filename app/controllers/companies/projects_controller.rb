f9455310a2aecdda88ed19131e5cf25e18ee82bd
victor de spirlet <victordespirlet@gmail.com>
2022-11-30 12:13:10 +0100

    seeds de merde

--- app/controllers/companies/projects_controller.rb
+++ app/controllers/companies/projects_controller.rb
@@ -1,6 +1,5 @@
 module Companies
   class ProjectsController < ApplicationController
-    
     def index
       @projects = Project.all
     end
@@ -10,7 +9,7 @@ module Companies
     end
 
     private
-  
+
     def projects_params
       params.require(:project).permit(XXXX)
     end
