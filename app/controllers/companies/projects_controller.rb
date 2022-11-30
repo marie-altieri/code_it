fe8772762b4d54de84b01ab160166d8725420c6b
Daphne Lefevre <daph.lefevre@gmail.com>
2022-11-30 12:34:12 +0100

    dashboard available

--- app/controllers/companies/projects_controller.rb
+++ app/controllers/companies/projects_controller.rb
@@ -16,18 +16,18 @@ module Companies
     def create
       @project = Project.new(projects_params)
       @project.company_id = current_user.id
-      
+
       if @project.save
-        redirect_to companies_projects_path
+        redirect_to coders_projects_path
       else
-        render "companies/projects/new"
+        render "companies/projects/new", status: :unprocessable_entity
       end
     end
 
     private
 
     def projects_params
-      params.require(:project).permit(:company_id, :description, :deadline)
+      params.require(:project).permit(:coder_id, :description, :deadline)
     end
   end
 end
