6c61536379468b0e198249535ea5e369edc09004
victor de spirlet <victordespirlet@gmail.com>
2022-11-30 14:30:29 +0100

    search bar done

--- app/controllers/companies/users_controller.rb
+++ app/controllers/companies/users_controller.rb
@@ -2,6 +2,12 @@ module Companies
   class UsersController < ApplicationController
     def index
       @coders = User.all.where(role: "Coder")
+      if params[:query].present?
+        sql_query = "first_name ILIKE :query OR username ILIKE :query OR language ILIKE :query"
+        @coders = User.where(sql_query, query: "%#{params[:query]}%")
+      else
+        @coders = User.all
+      end
     end
 
     def show
@@ -11,7 +17,7 @@ module Companies
     private
 
     def users_params
-      params.require(:user).permit(XXXX)
+      params.require(:user).permit(:first_name, :last_name, :username)
     end
   end
 end
