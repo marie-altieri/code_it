2f770ca81bc1713ea68a5e990093d9b799fd6a7c
Daphne Lefevre <daph.lefevre@gmail.com>
2022-12-01 15:06:15 +0100

    change coder_id in coder name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one_attached :photo
   validates :first_name, :last_name, :username, :email, :photo, :password, :password_confirmation, :availability, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
