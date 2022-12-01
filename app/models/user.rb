756144d9a81aeabcb2a394c8f2359bfdc031d374
victor de spirlet <victordespirlet@gmail.com>
2022-12-01 13:54:17 +0100

    lot of changes for sign up, and small updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-         has_one_attached :photo
-
+  has_one_attached :photo
+  validates :first_name, :last_name, :username, :email, :photo, :password, :password_confirmation, :availability, presence: true
 end
