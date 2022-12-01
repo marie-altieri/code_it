d74f6bc67a641619268115567591e18392507c9c
Daphne Lefevre <daph.lefevre@gmail.com>
2022-12-01 15:20:24 +0100

    seeds fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Destroying all projects"
+Project.destroy_all
 puts "Destroying all users................"
 User.destroy_all
 puts "Creating coders........"
