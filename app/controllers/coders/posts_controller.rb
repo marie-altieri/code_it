4206bb516c154625d2b00ce46ebecc447701988a
victor de spirlet <victordespirlet@gmail.com>
2022-12-08 15:01:46 +0100

    barre de recherche update

--- app/controllers/coders/posts_controller.rb
+++ app/controllers/coders/posts_controller.rb
@@ -1,7 +1,19 @@
 module Coders
   class PostsController < ApplicationController
+    # def index
+    #   @posts = Post.all
+    # end
+
     def index
       @posts = Post.all
+      if params[:query].present?
+        sql_query = "posts.name ILIKE :query OR posts.description ILIKE :query OR users.first_name ILIKE :query"
+        @posts = Post.where(sql_query, query: "%#{params[:query]}%")
+
+        @posts = Post.joins(:company).where(sql_query, query: "%#{params[:query]}%")
+      else
+        @posts
+      end
     end
 
     def show
